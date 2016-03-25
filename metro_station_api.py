cf1c04f98e5ef3e6a132ad67a43f1957cdbb9189
Jeremy Castle <jeremy.r.castle@gmail.com>
2016-03-24 20:59:06 -0400

    Update metro_station_api.py

--- metro_station_api.py
+++ metro_station_api.py
@@ -5,7 +5,7 @@ import json
 
 #api_key = fb7119a0d3464673825a26e94db74451
 
-url = "https://api.wmata.com/Rail.svc/json/jStationEntrances?Lat=38.8978168&Lon=-77.0404246&Radius=5000&api_key=fb7119a0d3464673825a26e94db74451"
+url = "https://api.wmata.com/Rail.svc/json/jStationEntrances?api_key=fb7119a0d3464673825a26e94db74451"
 
 connection_pool = urllib3.PoolManager()
 json_obj = connection_pool.request('GET', url )
